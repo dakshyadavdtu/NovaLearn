992d8ca0f8b34763e04662e95133abab43633b6e
Daksh Yadav <dakshyadavdtu@gmail.com>
2026-02-25 23:02:07 +0530

    bootstrap auth on refresh

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,9 @@ import { Routes, Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { setUser } from './redux/userSlice';
+import { setUser, setLoading } from './redux/userSlice';
 import * as authApi from './api/auth';
+import ProtectedRoute from './components/ProtectedRoute';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
@@ -13,16 +14,18 @@ function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
+    dispatch(setLoading(true));
     authApi
       .getMe()
       .then(({ user }) => dispatch(setUser(user))
-      .catch(() => dispatch(setUser(null)));
+      .catch(() => dispatch(setUser(null)))
+      .finally(() => dispatch(setLoading(false)));
   }, [dispatch]);
 
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
